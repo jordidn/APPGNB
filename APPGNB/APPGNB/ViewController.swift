8f1a7285e5f1843732db2a0cf6adbf0e28f799ea
Jordi Duran <jordi.duran@movetia.com>
2020-09-04 19:24:08 +0200

    init project

---
+++ APPGNB/APPGNB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  APPGNB
+//
+//  Created by Jordi Durán on 04/09/2020.
+//  Copyright © 2020 Jordi Duran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
